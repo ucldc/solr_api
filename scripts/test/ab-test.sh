68ce0acc1a8b41810c089ff0ad1a7cefecf15fb6
Mark Redar <mredar@gmail.com>
2015-10-29 14:07:52 -0700

    return some rows in ab test.

--- scripts/test/ab-test.sh
+++ scripts/test/ab-test.sh
@@ -1,5 +1,5 @@
 echo "auth_token"
 read auth_token
 
-ab -c 30 -n 2000 -H "X-Authentication-Token: $auth_token" 'https://ucldc-solr-stage-loadtest.elasticbeanstalk.com/solr/query?q=collection_url:%22https://registry.cdlib.org/api/v1/collection/26094/%22&facet=true&facet.query=true&facet.field=reference_image_md5&rows=0' &
-ab -c 30 -n 2000 -H "X-Authentication-Token: $auth_token" 'https://ucldc-solr-stage-loadtest.elasticbeanstalk.com/solr/dc-collection/select?q=*:*&rows=0&facet=true&facet.limit=1000&facet.field=title_ss' &
+ab -c 30 -n 2000 -H "X-Authentication-Token: $auth_token" 'https://ucldc-solr-stage-loadtest.elasticbeanstalk.com/solr/query?q=collection_url:%22https://registry.cdlib.org/api/v1/collection/26094/%22&facet=true&facet.query=true&facet.field=reference_image_md5' &
+ab -c 30 -n 2000 -H "X-Authentication-Token: $auth_token" 'https://ucldc-solr-stage-loadtest.elasticbeanstalk.com/solr/dc-collection/select?q=*:*&facet=true&facet.limit=1000&facet.field=title_ss' &
