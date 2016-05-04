fabe01b911ec05d3fb633f896056d7b5cd15b1ee
Mark Redar <mredar@gmail.com>
2016-05-04 13:39:31 -0700

    change to symbolic name for solr server

--- scripts/test/queries-stemming.sh
+++ scripts/test/queries-stemming.sh
@@ -1,4 +1,4 @@
-#"https://52.10.100.133/solr/dc-collection/select?q=d%C3%ADa&wt=json"
+#"https://harvest-stg.cdlib.org/solr/dc-collection/select?q=d%C3%ADa&wt=json"
 echo "enter X-Authentication-Token"
 read auth_token
 echo "enter username:password"
@@ -13,7 +13,7 @@ ucldc_num_found_run=`curl -s -k -H "X-Authentication-Token: $auth_token" \
 echo "UCLDC beanstalk  select \"run\" found $ucldc_num_found_run"
 new_index_num_found=`curl -s -k -H "X-Authentication-Token: $auth_token" \
     --digest --user $auth \
-   "https://52.10.100.133/solr/dc-collection/select?q=run&wt=json" | \
+   "https://harvest-stg.cdlib.org/solr/dc-collection/select?q=run&wt=json" | \
    jq '.response.numFound'`
 echo "NEW Index select \"run\" found $new_index_num_found"
 echo "================================================================================"
@@ -24,7 +24,7 @@ ucldc_num_found_runs=`curl -s -k -H "X-Authentication-Token: $auth_token" \
 echo "UCLDC beanstalk  select \"runs\" found $ucldc_num_found_runs"
 new_index_num_found=`curl -s -k -H "X-Authentication-Token: $auth_token" \
     --digest --user $auth \
-   "https://52.10.100.133/solr/dc-collection/select?q=runs&wt=json" | \
+   "https://harvest-stg.cdlib.org/solr/dc-collection/select?q=runs&wt=json" | \
    jq '.response.numFound'`
 echo "NEW Index select \"runs\" found $new_index_num_found"
 echo "================================================================================"
@@ -35,7 +35,7 @@ ucldc_num_found_running=`curl -s -k -H "X-Authentication-Token: $auth_token" \
 echo "UCLDC beanstalk  select \"running\" found $ucldc_num_found_running"
 new_index_num_found=`curl -s -k -H "X-Authentication-Token: $auth_token" \
     --digest --user $auth \
-   "https://52.10.100.133/solr/dc-collection/select?q=running&wt=json" | \
+   "https://harvest-stg.cdlib.org/solr/dc-collection/select?q=running&wt=json" | \
    jq '.response.numFound'`
 echo "NEW Index select \"running\" found $new_index_num_found"
 echo "================================================================================"
